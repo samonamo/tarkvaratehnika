85e6bf0666f169be55c2bdf00a2dbc8693578cec
MATHIAS <mathias.liias@ttu.ee>
2018-02-28 00:12:15 +0200

    Spring gradle project

---
+++ WebGradle/src/main/java/com/juuksurisalong/web/WebGradleApplication.java
@@ -0,0 +1,12 @@
+package com.juuksurisalong.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGradleApplication.class, args);
+	}
+}
