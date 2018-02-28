6161c9ad6c4bbf4793047f067cf5d0f22a4aa832
mathias.liias <mathias.liias@ICT-12218K.intra.ttu.ee>
2018-02-28 11:10:16 +0200

    Working db test config

--- WebGradle/src/main/java/com/juuksurisalong/web/Application.java
+++ WebGradle/src/main/java/com/juuksurisalong/web/Application.java
@@ -3,7 +3,6 @@ package com.juuksurisalong.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 
---
+++ WebGradle/src/main/java/com/juuksurisalong/web/data/Users.java
@@ -0,0 +1,35 @@
+package com.juuksurisalong.web.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Users {
+	@Id
+	@GeneratedValue
+	long id;
+	String firstName;
+	String lastName;
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+}
