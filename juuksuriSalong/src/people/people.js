b1bbace52d8cb015481b25fc13712aeb81906b1a
Karl <kajams@ttu.ee>
2018-03-06 21:59:30 +0200

    Request to DB from Aurelia
    Now when registered from website saves correctly data to DB. Aurelia can also get correct users data from DB.

--- juuksuriSalong/src/people/people.js
+++ juuksuriSalong/src/people/people.js
@@ -3,23 +3,35 @@ import {HttpClient, json} from 'aurelia-fetch-client'
 export class people{
 
 	userData = {}
+	userList = []
 
 	constructor()	{
 		this.appName = "juuksuriSalong"
 		this.count = 0
 	}
 
+	activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/service/users/all')
+			.then(response => response.json())
+			.then(users => this.userList = users);
+	}
+
 	addUser() {
 		let client = new HttpClient();
 
 		// let userData = {
+		// 	"email":"testemail@gmail.com",
+		//	"password":"testpw123"
 		// 	"firstName": "MyName",
 		// 	"lastName": "MyLastName",
-		// 	"email":"testemail@gmail.com",
 		// 	"phoneNumber":"551231231",
 		// }
+
+		
 		console.log("Selle ma saadan serverile", json(this.userData))
-		client.fetch('http://localhost:8080/greeting', {
+		client.fetch('http://localhost:8080/service/users/register', {
 			'method': "POST",
 			'body': json(this.userData)
 			// 'body':"midagi"
