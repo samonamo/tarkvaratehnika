f3c15e39f82fd8c6528c2c1f86c1bb3fccf14bad
arjusk <arjusk@ttu.ee>
2018-03-28 22:01:08 +0300

    Add React version with working POST.

--- salong/src/Main.js
+++ salong/src/Main.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
 import SignIn from "./components/bodyComponents/SignIn";
 import SignUp from "./components/bodyComponents/SignUp";
+import {Link, Route, BrowserRouter} from "react-router-dom";
 
 class Main extends Component {
 
     render() {
         return (
-            <div>
-                <SignIn />
-                <SignUp />
-            </div>
+
+            <BrowserRouter>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/SignIn">Sign in</Link></li>
+                        <li><Link to="/SignUp">Sign up</Link></li>
+                    </ul>
+
+                    <Route exact path="/" component={Main}/>
+                    <Route path="/SignIn" component={SignIn}/>
+                    <Route path="/SignUp" component={SignUp}/>
+                </div>
+            </BrowserRouter>
+
         );
     }
 }
