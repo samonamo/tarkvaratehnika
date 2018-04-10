5ca762290f929ab7d1fcaf3ac4376581bb691315
Karl <kajams@ttu.ee>
2018-04-10 20:53:39 +0300

    Currently disabled booking

--- WebGradle/src/main/java/com/juuksurisalong/web/repositories/BookingRepository.java
+++ WebGradle/src/main/java/com/juuksurisalong/web/repositories/BookingRepository.java
@@ -1,11 +1,11 @@
-package com.juuksurisalong.web.repositories;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.juuksurisalong.web.data.Booking;
-
-public interface BookingRepository extends CrudRepository<Booking, Long> {
-	List<Booking> findAll();
-}
+//package com.juuksurisalong.web.repositories;
+//
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.juuksurisalong.web.data.Booking;
+//
+//public interface BookingRepository extends CrudRepository<Booking, Long> {
+//	List<Booking> findAll();
+//}
