4f4c072b96f010e6abfc4ff62d7abf7dd8f67f1a
arjusk <arjusk@ttu.ee>
2018-05-13 19:35:25 +0300

    Fixed slow calendar response, fixed timestamp.

--- salong/src/components/helpClasses/Calendar.js
+++ salong/src/components/helpClasses/Calendar.js
@@ -4,19 +4,29 @@ import events from './events';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
+BigCalendar.momentLocalizer(moment);
+
 let allViews = ['month', 'work_week', 'day'];
+const minTime = new Date();
+minTime.setHours(8, 0, 0);
+const maxTime = new Date();
+maxTime.setHours(18, 0, 0);
+
+const dateNow = new Date();
+
+const formats = {timeGutterFormat: 'HH:mm'};
 
 const Basic = () => (
-    <div style={{ height: 500, width:700 }}>
+    <div style={{height: 500, width: 700}}>
         <BigCalendar
+            formats={formats}
             events={events}
-            step={60}
+            step={30}
             views={allViews}
-            showMultiDayTimes
-            min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
-            max={new Date(2020, 0, 1, 17, 0)} // Max will be 6.00 PM!
-            defaultDate={new Date()}
+            min={minTime}
+            max={maxTime}
+            defaultDate={dateNow}
         />
     </div>
 );
