62a669e3dfc0a2c4aaf36deafde15684c8093411
MATHIAS <mathias.liias@ttu.ee>
2018-05-16 03:44:46 +0300

    Update index.js

--- salong/src/index.js
+++ salong/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from "./Main";
+import {createStore} from 'redux';
+import Provider from "react-redux/es/components/Provider";
+import AppReducer from "./appreducer/AppReducer";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+const store = createStore(AppReducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <BrowserRouter>
+      <Main/>
+    </BrowserRouter>
+  </Provider>
+), document.getElementById('root'));
